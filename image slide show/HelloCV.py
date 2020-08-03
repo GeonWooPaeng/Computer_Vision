a229eb4c5fa53d01fef36fdd94b8b3bfb9ec3e4f
GeonWooPaeng <gwpaeng@naver.com>
2020-08-03 15:45:52 +0900

    opencv image 불러오기

--- image slide show/HelloCV.py
+++ image slide show/HelloCV.py
@@ -3,16 +3,16 @@ import cv2
 
 print("Hello, OpenCV", cv2.__version__)
 
-#img 라는 영상을 만든다 
-img = cv2.imread('cat.bmp')
+#img 라는 영상 파일 불러오기(cat.bmp가 현재 파일에 존재해야한다.)
+img = cv2.imread('.\image slide show\cat.bmp')
 
 #예외처리(cat.bmp)에 대한
-if img in None:
+if img is None:
     print("Image load failed")
     sys.exit() 
 
-cv2.namedWindow('imgae') #창을 생성 
-cv2.imshow('image',img) #imgae창에다가 img의 영상을 보여준다.
-cv2.waitKey() #화면에 실제로 보여지게 한다.
+cv2.namedWindow('image') #image란 이름의 새 창을 생성 
+cv2.imshow('image',img) #image창에다가 img의 영상을 보여준다.
+cv2.waitKey() #키보드 입력이 있을 때까지 대기
 
-cv2.destroyAllWindows() #기존에 나타나 있는화면을 닫아라 
+cv2.destroyAllWindows() #생성된 모든 창을 닫는다.
