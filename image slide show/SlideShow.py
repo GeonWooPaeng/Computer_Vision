b3ea3d30d8b484423bea8e6b67369955e9eb8bbd
GeonWooPaeng <gwpaeng@naver.com>
2020-08-04 21:57:28 +0900

    slideShow to use picture

--- image slide show/SlideShow.py
+++ image slide show/SlideShow.py
@@ -2,35 +2,28 @@ import sys
 import glob
 import cv2
 
-
 # 이미지 파일을 모두 img_files 리스트에 추가
-img_files = glob.glob('.\\images\\*.jpg')
+img_files = glob.glob('.\image slide show\.\\images\\*.jpg')
 
+# img_files 유/무 확인 하기
 if not img_files:
     print("There are no jpg files in 'images' folder")
-    sys.exit()
+    sys.exit() 
 
-# 전체 화면으로 'image' 창 생성
+# 전체 화면 영상 출력하기 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.setWindowProperty('image',cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-# 무한 루프
 cnt = len(img_files)
-idx = 0
+idx = 0 
 
 while True:
-    img = cv2.imread(img_files[idx])
-
-    if img is None:
-        print('Image load failed!')
-        break
-
+    img = cv2.imread(img_files[idx%cnt])
     cv2.imshow('image', img)
-    if cv2.waitKey(1000) >= 0:
-        break
 
-    idx += 1
-    if idx >= cnt:
-        idx = 0
+    if cv2.waitKey(1000) >= 27: #ESC 
+        break 
 
-cv2.destroyAllWindows()
+    idx+=1 
+
+cv2.destroyAllWindows() 
