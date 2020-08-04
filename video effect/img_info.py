8b226770187a84db55179e32a481a90ac0b830a0
GeonWooPaeng <gwpaeng@naver.com>
2020-08-04 22:31:26 +0900

    영상속성 및 픽셀 값 참조

--- video effect/img_info.py
+++ video effect/img_info.py
@@ -12,15 +12,19 @@ if img1 is None or img2 is None:
 
 # 영상의 속성 참조
 print('type(img1):', type(img1))
-print('img1.shape:', img1.shape)
+print('img1.shape:', img1.shape) #(세로,가로)
 print('img2.shape:', img2.shape)
 print('img1.dtype:', img1.dtype)
+print('img2.dtype:', img2.dtype)
+
 
 # 영상의 크기 참조
 h, w = img2.shape[:2]
-print('img2 size: {} x {}'.format(w, h))
+print('img2 size(w x h): {} x {}'.format(w, h))
 
-if len(img1.shape) == 2:
+
+# gray scale 확인하기 
+if len(img1.shape) == 2: #img1.ndim도 2 출력
     print('img1 is a grayscale image')
 elif len(img1.shape) == 3:
     print('img1 is a truecolor image')
@@ -29,14 +33,19 @@ cv2.imshow('img1', img1)
 cv2.imshow('img2', img2)
 cv2.waitKey()
 
-# 영상의 픽셀 값 참조
-for y in range(h):
-    for x in range(w):
-        img1[y, x] = 255
-        img2[y, x] = (0, 0, 255)        
 
-# img1[:,:] = 255
-# img2[:,:] = (0, 0, 255)
+# 영상의 픽셀 값 참조
+# p1 = img1[y,x] => img1의 (x,y) 좌표에 있는 값
+# p2 = img2[y,x] => img2의 (x,y) 좌표에 있는 값([블루, 그린, 레드])
+
+#영상의 픽셀 값을 setting하는 것(오래걸리므로 하지 말것)
+# for y in range(h):
+#     for x in range(w):
+#         img1[y, x] = 255 #검정으로 setting
+#         img2[y, x] = (0, 0, 255) #빨강색으로 setting  
+
+img1[:,:] = 255 #검정으로 setting
+img2[:,:] = (0, 0, 255) #빨강색으로 setting
 
 cv2.imshow('img1', img1)
 cv2.imshow('img2', img2)
