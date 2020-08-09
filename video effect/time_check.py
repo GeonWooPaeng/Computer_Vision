ef7b7765f3a2052f9ab4b7cbef187bf0269bf948
GeonWooPaeng <gwpaeng@naver.com>
2020-08-09 15:12:36 +0900

    time_Check

--- video effect/time_check.py
+++ video effect/time_check.py
@@ -1,20 +1,23 @@
+# 보통 30ms 이하로 되야지 괜찮은 것이다.
+# 20번 정도 돌려서 시간 check해야 한다.
+
 import sys
-import time
+import time #파이썬 기능
 import numpy as np
-import cv2
+import cv2 #opencv 기능
 
 
-img = cv2.imread('hongkong.jpg')
+img = cv2.imread('.\\video effect.\hongkong.jpg')
 
 tm = cv2.TickMeter()
 
-tm.reset()
-tm.start()
+tm.reset() #시간 측정 초기화
+tm.start() #시간 측정 시작
 t1 = time.time()
 
 edge = cv2.Canny(img, 50, 150)
 
-tm.stop()
+tm.stop() #시간 측정 끝
 print('time:', (time.time() - t1) * 1000)
-print('Elapsed time: {}ms.'.format(tm.getTimeMilli()))
+print('Elapsed time: {}ms.'.format(tm.getTimeMilli())) #strat 부터 stop까지의 시간
 
