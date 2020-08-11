f33683c62aba382629498cd974a2877e9bcb26e8
GeonWooPaeng <gwpaeng@naver.com>
2020-08-11 15:20:55 +0900

    histogram

--- chroma-key synthesis/histogram1.py
+++ chroma-key synthesis/histogram1.py
@@ -1,3 +1,7 @@
+# 히스토그램
+# 영상의 픽셀 값 분포를 그래프의 형태로 표현한 것 
+# ex) 그레이스케일 영상에서 각 그레이스케일 값에 해당하는 픽셀의 개수를 구하고, 이를 막대 그래프의 형태로 표현 
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,22 +9,30 @@ import cv2
 
 
 # 그레이스케일 영상의 히스토그램
-src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\chroma-key synthesis.\lenna.bmp', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
     print('Image load failed!')
     sys.exit()
+
+# cv2.calcHist(img, channels, mask, histSize, ranges, hist=None, accumulate=None)
+# channels: [0]이면 gray_scale, [0,1]이면 blue + green, [0,1,2]이면 blue + gree + red 
+# mask: None이면 입력 영상 전체에서 히스토그램을 구함
+# histSize: [256]은 0부터 25까지 받겠다. 히스토그램 각 차원의 크기(빈(bin)의 개수)를 나타내는 리스트
+# ranges: 히스토그램 각 차원의 최솟값과 최댓값 
+# hist: 계산된 히스토그램
+# accumulate: 누적이면 True, 새로 만들면 False
 
 hist = cv2.calcHist([src], [0], None, [256], [0, 256])
 
 cv2.imshow('src', src)
 cv2.waitKey(1)
 
-plt.plot(hist)
+plt.plot(hist) #1차원 그래프를 편하게 보여준다.
 plt.show()
 
 # 컬러 영상의 히스토그램
-src = cv2.imread('lenna.bmp')
+src = cv2.imread('.\chroma-key synthesis.\lenna.bmp')
 
 if src is None:
     print('Image load failed!')
--- chroma-key synthesis/histogram2.py
+++ chroma-key synthesis/histogram2.py
@@ -5,18 +5,17 @@ import cv2
 
 
 def getGrayHistImage(hist):
-    imgHist = np.full((100, 256), 255, dtype=np.uint8)
+    imgHist = np.full((100, 256), 255, dtype=np.uint8) #가로 100 세로 256인 흰색 그림을 만든다.
 
     histMax = np.max(hist)
     for x in range(256):
         pt1 = (x, 100)
-        pt2 = (x, 100 - int(hist[x, 0] * 100 / histMax))
-        cv2.line(imgHist, pt1, pt2, 0)
+        pt2 = (x, 100 - int(hist[x, 0] * 100 / histMax)) #코드가 max값을 넘어가지 않게 하는 곳
+        cv2.line(imgHist, pt1, pt2, 0) #밑에서 위로 선을 그어주고 있다.
 
     return imgHist
 
-
-src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\chroma-key synthesis.\lenna.bmp', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
     print('Image load failed!')
