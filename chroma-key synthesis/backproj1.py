6941ff4a112cb359915252698725ca2b1de85f8a
GeonWooPaeng <gwpaeng@naver.com>
2020-08-12 17:51:24 +0900

    히스토그램 inrange방법 & 역투영방법

--- chroma-key synthesis/backproj1.py
+++ chroma-key synthesis/backproj1.py
@@ -1,3 +1,10 @@
+#히스토그램 inrange방법
+# 범위를 주어 그 범위에 해당하는 부분만 추출한다.
+
+# 실행 방법
+#1. 추출하고 싶은 색 부분을 마우스로 드래그 한다.
+#2. enter key를 누른다.
+
 import sys
 import numpy as np
 import cv2
@@ -5,31 +12,40 @@ import cv2
 
 # 입력 영상에서 ROI를 지정하고, 히스토그램 계산
 
-src = cv2.imread('cropland.png')
+src = cv2.imread('.\chroma-key synthesis.\cropland.png')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
-x, y, w, h = cv2.selectROI(src)
 
+#ROI selector이다 즉 마우스를 이용하여 영역을 드래그하여 사용할 수 있다.
+x, y, w, h = cv2.selectROI(src) 
+
+
+#히스토그램을 계산하는 코드 ----
 src_ycrcb = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)
 crop = src_ycrcb[y:y+h, x:x+w]
 
-channels = [1, 2]
-cr_bins = 128
+channels = [1, 2] #0번은 gray 값이므로 사용하지 않는다. => 밝기 정보이기 때문
+cr_bins = 128 
 cb_bins = 128
 histSize = [cr_bins, cb_bins]
 cr_range = [0, 256]
 cb_range = [0, 256]
 ranges = cr_range + cb_range
 
+#히스토그램 계산
 hist = cv2.calcHist([crop], channels, None, histSize, ranges)
+#-----------
+
+
 hist_norm = cv2.normalize(cv2.log(hist+1), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
 # 입력 영상 전체에 대해 히스토그램 역투영
 
-backproj = cv2.calcBackProject([src_ycrcb], channels, hist, ranges, 1)
+
+backproj = cv2.calcBackProject([src_ycrcb], channels, hist, ranges, 1) # 내가 원하는 부분만 골라내는 곳
 dst = cv2.copyTo(src, backproj)
 
 cv2.imshow('backproj', backproj)
--- chroma-key synthesis/backproj2.py
+++ chroma-key synthesis/backproj2.py
@@ -1,11 +1,21 @@
+# 히스토그램 역투영
+# 임의의 색상 영역을 검출할때 효과적 
+# ycrcb를 주로 사용 
+
+# 히스토그램 역투영을 이용하여 살색을 검출 => 범위가 아니라 내가 원하는 부분대로 작성 가능하다
+# mask를 이용하여 추출하고 싶은 부분(흰색부분)을 추출하고 그것을 가지고 다른 영상에 사용하는 방법
+
+# 1. 기준 영상에서 살색에 대한 컬러 히스토그램을 미리 계산
+# 2. 입력 영상에서 미리 구한 살색 히스토그램에 부합하는 픽셀을 선별 
+
 import sys
 import numpy as np
 import cv2
 
 
 # CrCb 살색 히스토그램 구하기
-ref = cv2.imread('kids1.png', cv2.IMREAD_COLOR)
-mask = cv2.imread('kids1_mask.bmp', cv2.IMREAD_GRAYSCALE)
+ref = cv2.imread('.\chroma-key synthesis.\kids1.png', cv2.IMREAD_COLOR)
+mask = cv2.imread('.\chroma-key synthesis.\kids1_mask.bmp', cv2.IMREAD_GRAYSCALE)
 
 if ref is None or mask is None:
     print('Image load failed!')
@@ -15,12 +25,16 @@ ref_ycrcb = cv2.cvtColor(ref, cv2.COLOR_BGR2YCrCb)
 
 channels = [1, 2]
 ranges = [0, 256, 0, 256]
+
+# 히스토그램을 가져다가 출력을 해주는 것, 특정 mask영역에서 히스토그램을 구하고 싶다.(2차원 형태)
 hist = cv2.calcHist([ref_ycrcb], channels, mask, [128, 128], ranges)
+
+# hist를 화면으로 보고 싶어 gray_scale영상으로 변환 / log로 해주는 것이 좋다.
 hist_norm = cv2.normalize(cv2.log(hist + 1), None, 0, 255, 
                           cv2.NORM_MINMAX, cv2.CV_8U)
 
 # 입력 영상에 히스토그램 역투영 적용
-src = cv2.imread('kids2.png', cv2.IMREAD_COLOR)
+src = cv2.imread('.\chroma-key synthesis.\kids2.png', cv2.IMREAD_COLOR)
 
 if src is None:
     print('Image load failed!')
@@ -28,10 +42,11 @@ if src is None:
 
 src_ycrcb = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)
 
-backproj = cv2.calcBackProject([src_ycrcb], channels, hist, ranges, 1)
+#calcHist에서 계산된 값을 입력으로 받아 확률과 같은 img로 반환해준다./ gray_scale형태이므로 그냥 imshow해도 된다.
+backproj = cv2.calcBackProject([src_ycrcb], channels, hist, ranges, 1) 
 
 cv2.imshow('src', src)
 cv2.imshow('hist_norm', hist_norm)
-cv2.imshow('backproj', backproj)
+cv2.imshow('backproj', backproj) 
 cv2.waitKey()
 cv2.destroyAllWindows()
