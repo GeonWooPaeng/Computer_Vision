7fd254572ca491fa0b6cb2942cfea189ae4c7102
GeonWooPaeng <gwpaeng@naver.com>
2020-08-12 17:29:53 +0900

    원색 영역 추출

--- chroma-key synthesis/inrange1.py
+++ chroma-key synthesis/inrange1.py
@@ -1,18 +1,23 @@
+# 특정 색상 영역 추출 
+# RGB는 영상의 밝기에 따라서 잘 추출되지 않는다. 
+# 그러나 hsv는 밝기에 상관 없이 잘 추출된다.
 import sys
 import numpy as np
 import cv2
 
 
-src = cv2.imread('candies.png')
-#src = cv2.imread('candies2.png')
+src = cv2.imread('.\chroma-key synthesis.\candies.png')
+#src = cv2.imread('.\chroma-key synthesis.\candies2.png')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
-src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV) #hsv 로 변환
 
-dst1 = cv2.inRange(src, (0, 128, 0), (100, 255, 100))
+#inRange(src, lowerb, upperb, dst=None)
+# lowerb(하한 값) ~ upperb(상한 값) 사이 값만 골라서 dst형태로 return 해준다.
+dst1 = cv2.inRange(src, (0, 128, 0), (100, 255, 100)) #(h,s,v) 값
 dst2 = cv2.inRange(src_hsv, (50, 150, 0), (80, 255, 255))
 
 cv2.imshow('src', src)
--- chroma-key synthesis/inrange2.py
+++ chroma-key synthesis/inrange2.py
@@ -1,20 +1,26 @@
+# hsv - 원색을 찾을 때 좋다
+# h: 각도로 색을 나타낸다 but 360도 전체로 하는 것이 아니라 180도를 전체 원으로 한다.(90 -> 45)
+# s: 원 중심(0), 테두리가 255이다.
+# v: 밝기
 import sys
 import numpy as np
 import cv2
 
 
-src = cv2.imread('candies.png')
+src = cv2.imread('.\chroma-key synthesis.\candies.png')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
-src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV) #hsv로 변환
 
 def on_trackbar(pos):
-    hmin = cv2.getTrackbarPos('H_min', 'dst')
+    hmin = cv2.getTrackbarPos('H_min', 'dst') #dst 창에 있는 H_min trackbar의 위치를 받아오는 함수 
     hmax = cv2.getTrackbarPos('H_max', 'dst')
 
+    #inRange(src, lowerb, upperb, dst=None)
+# lowerb(하한 값) ~ upperb(상한 값) 사이 값만 골라서 dst형태로 return 해준다.
     dst = cv2.inRange(src_hsv, (hmin, 150, 0), (hmax, 255, 255))
     cv2.imshow('dst', dst)
 
@@ -22,8 +28,8 @@ def on_trackbar(pos):
 cv2.imshow('src', src)
 cv2.namedWindow('dst')
 
-cv2.createTrackbar('H_min', 'dst', 50, 179, on_trackbar)
-cv2.createTrackbar('H_max', 'dst', 80, 179, on_trackbar)
+cv2.createTrackbar('H_min', 'dst', 50, 179, on_trackbar) #H값의 lowerb
+cv2.createTrackbar('H_max', 'dst', 80, 179, on_trackbar) #H값의 upperb
 on_trackbar(0)
 
 cv2.waitKey()
